b842dad1f53fd7f3d2485567dbb3f974c735f73c
tverdone10 <tverdone10@gmail.com>
2021-05-04 15:23:25 -0700

    intializing the project, general structure all set up

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+const PORT = 5000;
+
+// Store the connection string here (Better to have it in an env file, but just leaving it here for demonstration purposes)
+const atlas_uri = 'mongodb+srv://newUser:newUserPassword@cluster0.utqse.mongodb.net/UserSignup?retryWrites=true&w=majority'
+
+// Set up MongoDB Atlas Connection
+mongoose.connect(atlas_uri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+});
+
+// Connect to our database
+const connection = mongoose.connection
+connection.once("open", () => {
+    console.log("MongoDB Atlas connection as been established!")
+})
+
+// Connect our routes to the server here
+const signup = require("./routes/signup")
+app.use("/api/signup", signup)
+
+// Start the server
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
